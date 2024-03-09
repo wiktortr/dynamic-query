e759df5d0ea22e780d56c6361d934f63f203cdf0
Wiktor Trybulec <wiktortr9@gmail.com>
2024-03-09 13:47:33 +0100

    Inited Spring Boot application

---
+++ src/main/java/com/example/dynamicquery/DynamicQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dynamicquery/DynamicQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamicquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
